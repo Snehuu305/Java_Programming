d379034ca0ccb4da775e5829ac04cdda1d779ce7
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-07-02 21:44:49 +0530

    Add files via upload

---
+++ arrays/SortedSquares.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int[] sortedSquare(int[] nums)
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums[i] *= nums[i];
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+class SortedSquares
+{
+    public static void main(String A[])
+    {
+
+    }
+}
