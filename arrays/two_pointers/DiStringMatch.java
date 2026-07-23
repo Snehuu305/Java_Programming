c050e173094f17040f74cb29ecf25c7c26d86a27
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-07-23 22:37:41 +0530

    Add files via upload

---
+++ arrays/two_pointers/DiStringMatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+
+        char[] ch = s.toCharArray();
+
+        int n = ch.length;
+        int[] ans = new int[n + 1];
+
+        int low = 0;
+        int high = n;
+
+        for (int i = 0; i < n; i++) {
+
+            if (ch[i] == 'I') {
+                ans[i] = low;
+                low++;
+            } else {
+                ans[i] = high;
+                high--;
+            }
+        }
+
+        ans[n] = low;
+
+        return ans;
+    }
+}
