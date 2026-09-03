ee1de3983fb905fe58e3f87f644e54a3a00b2b91
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-09-03 22:40:45 +0530

    Refactor rotateArray.java for clarity

--- arrays/two_pointers/rotateArray.java
+++ arrays/two_pointers/rotateArray.java
@@ -5,12 +5,13 @@ class Solution {
         int n = arr.length;
         k = k % n;
 
-        reverse(arr, 0, n - 1);      // reverse whole array
+        reverse(arr, 0, n - 1);      // reverse whole arra
         reverse(arr, 0, k - 1);      // reverse first k elements
         reverse(arr, k, n - 1);      // reverse remaining
     }
 
-    private void reverse(int[] arr, int left, int right) {
+    private void reverse(int[] arr, int left, int right) 
+    {
         while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
