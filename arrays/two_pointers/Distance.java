5db241ae60e17c5abf25d6dd160b25a2a3342d97
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-09-21 22:41:52 +0530

    Update Distance.java

--- arrays/two_pointers/Distance.java
+++ arrays/two_pointers/Distance.java
@@ -5,7 +5,8 @@
 //         long arr[] = new long[nums.length];
 
 //         for(int i = 0; i < nums.length; i++) {
-//             for(int j = i+1; j < nums.length; j++){
+//             for(int j = i+1; j < nums.length; j++)
+{
 //                if(nums[i] == nums[j]) {
 //                     arr[i] += Math.abs(i-j); 
 //                 } 
